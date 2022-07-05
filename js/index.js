a3785c24eacf254fb16ae3b3132e0cb3f0737cfd
s-matsubara <mtytsy15301217@gmail.com>
2022-07-05 16:07:35 +0900

    aaa

--- js/index.js
+++ js/index.js
@@ -1,31 +1,4 @@
 /*-------------------------------
-  新着求人情報 swiper
--------------------------------*/
-const newjobsSwiper = new Swiper('.newjobs__container', {
-
-  slidesPerView: 1,
-  spaceBetween: 12,
-  loop: true,
-  loopAdditionalSlides: 5,
-  speed: 500,
-  autoplay: {
-    delay: 3000
-  },
-
-  pagination: {
-    el: '.newjobs__pagination',
-    type: 'bullets'
-  },
-
-  navigation: {
-    nextEl: '.newjobs__button-next',
-    prevEl: '.newjobs__button-prev',
-  },
-
-});
-
-
-/*-------------------------------
   固定ヘッダー 表示 / 非表示
 -------------------------------*/
 function FixedAnime() {
