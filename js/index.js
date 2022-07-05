6eec2d27ec8c375f15dc055827a13121993e648a
s-matsubara <mtytsy15301217@gmail.com>
2022-07-05 13:45:06 +0900

    aaa

--- js/index.js
+++ js/index.js
@@ -23,3 +23,20 @@ const newjobsSwiper = new Swiper('.newjobs__container', {
   },
 
 });
+
+
+/*-------------------------------
+  固定ヘッダー 表示 / 非表示
+-------------------------------*/
+function FixedAnime() {
+  const mainVisualH = $('.mainVisual').outerHeight(true);
+  const scroll = $(window).scrollTop();
+  if (scroll >= mainVisualH) {
+    $('.header').addClass('visible');
+  } else {
+    $('.header').removeClass('visible');
+  }
+}
+$(window).scroll(function () {
+	FixedAnime();
+});
